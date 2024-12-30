48205f5be92d4e356378f521c7d61328bb4d1819
namhyun <namhyun3418@gmail.com>
2024-12-30 17:08:58 +0900

    백준 9498 문제 풀이

--- baekjoon/src/q9498/Main.java
+++ baekjoon/src/q9498/Main.java
@@ -37,5 +37,12 @@ public class Main {
 			default:
 				System.out.print("F");
 		}
+		/*
+		// case 3.
+	      if (score >= 90) System.out.println('A');
+	      else if (score >= 80) System.out.println('B');
+	      else if (score >= 70) System.out.println('C');
+	      else if (score >= 60) System.out.println('D');
+	      else System.out.println('F'); */
 	}
 }
