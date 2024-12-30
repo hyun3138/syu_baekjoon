d8293d3cf737aa3b6367d833ba156d4e99578ba6
namhyun <namhyun3418@gmail.com>
2024-12-30 14:38:07 +0900

    Initial commit

--- baekjoon/src/q2741/Main.java
+++ baekjoon/src/q2741/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
+		sc.close();
 		for(int i = 1; i <= num; i++) {
 			System.out.println(i);
 		}
