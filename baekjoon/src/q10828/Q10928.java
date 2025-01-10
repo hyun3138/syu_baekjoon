a6cd5d46bf6f0044936c2c3f94a1481e8c6b3c0d
SYU <SYU@DESKTOP-1VHBQ5H>
2025-01-10 16:04:33 +0900

    백준 #10828 스택 클래스를 구현

---
+++ baekjoon/src/q10828/Q10928.java
@@ -0,0 +1,71 @@
+package q10828;
+
+import java.util.Scanner;
+
+public class Q10928 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		Stack stk = new Stack(N);
+		for(int i=0; i<N; i++) {
+			String cmd = sc.next();
+			
+			switch(cmd) {
+			case "push":
+				int num = sc.nextInt();
+				stk.push(num);
+				break;
+			case "top":
+				stk.top();
+				break;
+			case "size":
+				stk.size();
+				break;
+			case "empty":
+				stk.empty();
+				break;
+			case "pop":
+				stk.pop();
+				break;
+			}
+		}
+		sc.close();
+	}
+}
+
+class Stack {
+	private int[] n;
+	private int top;
+	
+	public Stack(int p) {
+		n = new int[p];
+		top = -1;
+	}
+	public void push(int p) {
+		n[++top] = p;
+	}
+	public void top() {
+		if(top == -1) {
+			System.out.println(top);
+		} else {
+			System.out.println(n[top]);
+		}
+	}
+	public void size() {
+		System.out.println(top + 1);
+	}
+	public void empty() {
+		if (top == -1) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+	}
+	public void pop() {
+		if(top != -1) {
+			System.out.println(n[top--]);
+		} else {
+			System.out.println(top);
+		}
+	}
+}
