394c969930b2bfe4a5a3b788ad3389cab8832ccf
SYU <SYU@DESKTOP-1VHBQ5H>
2025-01-15 10:32:11 +0900

    백준 #1110 문제풀이

--- baekjoon/src/q1110/Main.java
+++ baekjoon/src/q1110/Main.java
@@ -6,9 +6,10 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+		sc.close();
+		
+		int oldNum = 0, newNum = N;
 		int cnt = 0;
-		int oldNum = 0, newNum = 0;
-
 		do {
 			cnt++;
 			int n2 = 0, n1 = 0;
@@ -17,7 +18,6 @@ public class Main {
 			n1 = oldNum % 10; // 1자리
 			newNum = (n1 * 10) + (n2 + n1) % 10;
 		} while (N != newNum);
-
 		System.out.println(cnt);
 	}
 }
